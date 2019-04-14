194417e370e3a9ed5961e05cdf71be4391f4acc5
RaphaelRoriz <49620582+RaphaelRoriz@users.noreply.github.com>
2019-04-14 18:18:45 -0300

    Create B.java
    classe B , para demonstração.

---
+++ B.java
@@ -0,0 +1,28 @@
+private class B{
+  private int B1;
+  provate double B2;
+  
+  public void setB1(int b){
+    B1 = a;
+  }
+  
+  public void setB2(double b){
+    B2 = a;
+  }
+  
+  public void getB1(){
+    return B1;
+  }
+  
+  public void getB2(){
+    return B2;
+  }
+  
+  public void MB1(){
+    System.out.print("MB1");
+  }
+  
+  public void MB2(){
+    System.out.print("MB2");
+  }
+}
