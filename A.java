de3b10d6c9cd933f64e951567d9466aad46c0096
RaphaelRoriz <49620582+RaphaelRoriz@users.noreply.github.com>
2019-04-14 18:17:07 -0300

    Create A.java
    Classe A , para demonstração

---
+++ A.java
@@ -0,0 +1,28 @@
+private class A{
+  private int A1;
+  provate double A2;
+  
+  public void setA1(int a){
+    A1 = a;
+  }
+  
+  public void setA2(double a){
+    A2 = a;
+  }
+  
+  public void getA1(){
+    return A1;
+  }
+  
+  public void getA2(){
+    return A2;
+  }
+  
+  public void MA1(){
+    System.out.print("MA1");
+  }
+  
+  public void MA2(){
+    System.out.print("MA2");
+  }
+}
