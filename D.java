51b6f3b4f85c4dd3cf919331bcac817f53416b0e
raphael f r roriz <raphael.roriz@estudante.ufla.br>
2019-04-15 22:08:13 -0300

    Criação de classe D

---
+++ D.java
@@ -0,0 +1,30 @@
+private class D{
+  private int D1;
+  provate int D2;
+  
+  public void setD1(int d){
+    D1 = d;
+  }
+  
+  public void setD2(double d){
+    D2 = d;
+  }
+  
+  public void getD1(){
+    return D1;
+  }
+  
+  public void getD2(){
+    return D2;
+  }
+  
+  public void MD1(){
+    System.out.print("MD1");
+  }
+  
+  public void MD2(){
+    System.out.print("MD2");
+  }
+
+
+}
